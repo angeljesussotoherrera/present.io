62bba28db308a7763bfaec634e39f11aef28bd47
Ángel de Jesús Soto Herrera <84299007+angeljesussotoherrera@users.noreply.github.com>
2021-06-02 08:29:04 -0500

    Add files via upload

---
+++ love.js
@@ -0,0 +1,22 @@
+var typed5 = new Typed('#typed5', {
+    strings: [
+    '<strong class="bb" style="color:#f11515;">ich liebe dich</strong>',
+    '<strong class="bb" style="color:#ebb117;">Je t aime</strong>',
+    '<strong class="bb" style="color:#37a4d1;">ég elska þig</strong>',
+    '<strong class="bb" style="color:#df8dac;">わたしは、あなたを愛しています</strong>',
+    '<strong class="bb" style="color:#f07126;">Jeg elsker deg</strong>',
+    '<strong class="bb" style="color:#d599fd;">jag älskar dig</strong>',
+    '<strong class="bb" style="color:#effb8c;">I love you</strong>',
+    '<strong class="bb" style="color:#e61d06;">Muchas formas existen...</strong>',
+    '<strong class="bb" style="color:#dbeff4;">Pero solo a ti te digo...</strong>',
+    '<strong class="bb" style="color:#fca4fc;">Te amo Patata &hearts;</strong>'
+      ],
+    typeSpeed: 100,
+    backSpeed: 50,
+    cursorChar: '|',
+    shuffle: false,
+    startDelay: 1500,
+    backDelay: 1000,
+    smartBackspace: false,
+    loop: true
+  });
